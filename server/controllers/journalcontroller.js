2ecb97d2d976704c7bb84a9d45cb2b4ee9a8914b
Medina88 <Msydyka@iu.edu>
2021-06-12 15:09:22 -0400

    /practice part adding

--- server/controllers/journalcontroller.js
+++ server/controllers/journalcontroller.js
@@ -1,8 +1,9 @@
 const Express = require("express"); //import the Express framework and store it inside of var
 const router = Express.Router(); //create new var, since the Express var gives us access in the express framework, we can access express properties 
 // and methods by calling express.method name(),therefore when we call express.router we are using express var to access the router() method
+const validateJWT = require ("../middleware/validate-jwt");
 
-router.get('/about', (req, res)=>{
+router.get('/practice', validateJWT, (req, res)=>{
     res.send('Hey!! This is the about route!') //path and anonymous call back function also called "handler function". Here the res parameter is a simple string
 });
 
